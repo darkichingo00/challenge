90bd9b67f9e2586c26d5694e82c3fd0460b3f746
Guillermo Hernandez <dark.hdez.2006@gmail.com>
2025-02-21 23:30:48 -0400

    enviroment3

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+// Sirve los archivos estáticos desde Angular
+app.use(express.static(path.join(__dirname, "/dist/WorkSync")));
+
+// Redirige todas las rutas al index.html
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "dist/WorkSync/browser/index.html"));
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`🚀 Servidor de Angular corriendo en el puerto ${PORT}`);
+});
